1dd4ba58a769cfa2c58b46ec56abe17a1d98c4d0
kekeli543 <kekeli543@gmail.com>
2021-06-09 16:37:13 +0800

    修改 api 路由表

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//文章
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('posts', 'App\Http\Controllers\Api\PostController');
+});
+//驗證路由
+Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Api'], function () {
+    Route::get('/', 'AuthController@me')->name('me');
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::post('logout', 'AuthController@logout')->name('logout');
 });
